6d0baef24e302ed79577db7e04d585a8b2df2323
standardrobot <newjonah12348@gmail.com>
2022-07-22 13:57:09 -0400

    init update

--- init.lua
+++ init.lua
@@ -1,5 +1,6 @@
 if vim.fn.has('macunix') then
     vim.cmd [[ luafile ~/.config/nvim/macos.lua]]
-elseif vim.fn.has('win32') then
+end
+if vim.fn.has('win32') then
     vim.cmd[[ luafile ~/.config/nvim/windows.lua]]
 end
