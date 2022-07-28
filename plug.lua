1f967e87d3182d770335a30b764c8877721407f6
Jonah Perry <newjonah12348@gmail.com>
2022-07-28 02:28:28 -0400

    updated documentation in plug.lua

--- plug.lua
+++ plug.lua
@@ -1,16 +1,18 @@
 local Plug = vim.fn['plug#']
 
 vim.call('plug#begin', '~/.local/share/nvim/plugged')
--- COLORSCHEMES
+-- Colorschemes
 Plug 'EdenEast/nightfox.nvim'
 Plug 'morhetz/gruvbox'
--- WORKFLOW PLUGINS
+
+-- LSP + bells n whistles (autocomplete, autopairs, etc.)
 Plug 'neovim/nvim-lspconfig'
 Plug 'hrsh7th/cmp-nvim-lsp'
 Plug 'hrsh7th/cmp-buffer'
 Plug 'hrsh7th/cmp-path'
 Plug 'hrsh7th/nvim-cmp'
 Plug 'windwp/nvim-autopairs'
+
 Plug 'nvim-treesitter/nvim-treesitter'
 Plug 'nvim-lua/popup.nvim'
 Plug 'nvim-lua/plenary.nvim'
