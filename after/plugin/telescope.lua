797e976dd9b9f5b8567860fbc549b4f6a9d84022
Jonah Perry <newjonah12348@gmail.com>
2022-08-06 21:22:47 -0500

    ok

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,18 +1,15 @@
 -- Functional wrapper for mapping custom keybindings
-function map(mode, lhs, rhs, opts)
+local map = function(mode, lhs, rhs, opts)
     local options = { noremap = true }
     if opts then
         options = vim.tbl_extend("force", options, opts)
     end
     vim.api.nvim_set_keymap(mode, lhs, rhs, options)
 end
--- nnoremap <silent> ;f <Cmd>Telescope find_files<CR>
--- nnoremap <silent> ;r <Cmd>Telescope live_grep<CR>
--- nnoremap <silent> \\ <Cmd>Telescope buffers<CR>
--- nnoremap <silent> ;; <Cmd>Telescope help_tags<CR>
 map("n", ";f", "<Cmd>Telescope find_files<CR>", { silent=true })
 map("n", ";r", "<cmd>Telescope live_grep<CR>", {silent=true})
 map("n", ";;", "<cmd>Telescope help_tags<CR>", {silent=true})
+map("n", ";b", "<cmd>Telescope buffers<CR>", {silent=true})
 
 local actions = require('telescope.actions')
 
