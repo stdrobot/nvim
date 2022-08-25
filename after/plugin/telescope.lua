550feeb6324232205b06fefabea2ebd4476d2bc7
astandardrobot <jonahjperry5@gmail.com>
2022-08-25 00:40:35 -0500

    telescope revisions

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -6,10 +6,10 @@ local map = function(mode, lhs, rhs, opts)
     end
     vim.api.nvim_set_keymap(mode, lhs, rhs, options)
 end
-map("n", ";f", "<Cmd>Telescope find_files<CR>", { silent=true })
-map("n", ";r", "<cmd>Telescope live_grep<CR>", {silent=true})
-map("n", ";;", "<cmd>Telescope help_tags<CR>", {silent=true})
-map("n", ";b", "<cmd>Telescope buffers<CR>", {silent=true})
+map("n", ";f", "<cmd>lua require('telescope.builtin').find_files({find_commands='rg'})<CR>", { silent=true })
+map("n", ";r", "<cmd>lua require('telescope.builtin').live_grep({find_commands='rg'})<CR>", {silent=true})
+map("n", ";;", "<cmd>lua require('telescope.builtin').help_tags()<CR>", {silent=true})
+map("n", ";b", "<cmd>lua require('telescope.builtin').buffers()<CR>", {silent=true})
 
 local actions = require('telescope.actions')
 
